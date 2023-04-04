6f212cc9de99c03441564e21f4c782ea24d801f5
naeemtalukder <naeemtalukder169135@gmail.com>
2023-04-05 01:20:10 +0600

    navbar done

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ allSideMenu.forEach(item=> {
 		li.classList.add('active');
 	})
 });
+
+
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sidebar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function () {
+    sidebar.classList.toggle('hide');
+})
